37deacf0913c4928aaa03b6c4c25001d73c1d5e9
Onwuasoanya James <onwuasoanyajames@gmail.com>
2024-11-28 10:58:30 +0100

    newsleta11/28 master(2)

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -4,29 +4,34 @@ import {  Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent 
 
 const SignUp = () => {
   return (
-    <div className=''>
+    <div className='p'>
+    <header className="p-5 justify-start flex">
+      <h1 className="font-Madetommy font-bold text-xl">adFeed</h1>    
+    </header>
+    <div className="p-5">
       <Card>
-          <CardHeader>
-            <CardTitle className='w-full text-base text-center'>Sign up</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
+        <CardHeader>
+          <CardTitle className='w-full text-base text-center'>Sign up</CardTitle>
+          <CardDescription>
+            Make changes to your account here. Click save when you're done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <label htmlFor="name">Name</label>
+            <input id="name" className='p-2 h-8 border border-gray-400 rounded' defaultValue="Pedro Duarte" />
+          </div>
+          <div className="space-y-1">
+            <label htmlFor="username">Username</label>
+            <input id="username" className='p-2 h-8 border border-gray-400 rounded' defaultValue="Pedro Duarte" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Save changes</Button>
+        </CardFooter>
+      </Card>
     </div>
+  </div>
   )
 }
 
